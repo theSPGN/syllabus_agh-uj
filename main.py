94efe9c2ba6e2008373cbceb7895ccc75980abc7
theSPGN <94869555+theSPGN@users.noreply.github.com>
2022-09-04 16:47:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -12,33 +12,33 @@ ctx.verify_mode = ssl.CERT_NONE
 def get_info(url):
     try:
         print(">>> ", url)
-        uh = urllib.request.urlopen(url,context=ctx)
+        uh = urllib.request.urlopen(url, context=ctx)
         data = uh.read().decode()
         return BeautifulSoup(data, 'html.parser')
-    except:
-        print("Error from get_info function")
+    except Exception as e:
+        print("Error from get_info function: ", e)
         quit()
 
 
-def get_data(soup):
+def get_data(my_data):
     try:
-        new_data = soup.find_all("div", {"class": "col-xs-12 col-sm-12 col-md-4 grid-element"})
+        look_for_data = my_data.find_all("div", {"class": "col-xs-12 col-sm-12 col-md-4 grid-element"})
         dic = dict()
-        for element in new_data:
+        for element in look_for_data:
             s = element.get_text(strip="\n")
             tag = element.find("a")
             t = tag.get("href")
-            x = 0
-            for i in range(len(t)):
-                if t[len(t)-i-1] == '/':
+            counter = 0
+            for a in range(len(t)):
+                if t[len(t) - a - 1] == '/':
                     break
-                x += 1
-            t = t[-x:]
+                counter += 1
+            t = t[-counter:]
             dic[t] = s
             print(t, s)
         return dic
-    except:
-        print("Error from get_data function")
+    except Exception as e:
+        print("Error from get_data function:", e)
         quit()
 
 
@@ -56,45 +56,57 @@ def write_data(my_data):
         subject = element.find_all('td')
         for e in subject:
             try:
-                x = e.find('div')
-                s += x.get_text(strip='\n') + '\n'
-            except:
+                tag = e.find('div')
+                s += tag.get_text(strip='\n') + '\n'
+            except AttributeError:
                 continue
     return s
 
 
-# strona agh:
-u = "https://sylabusy.agh.edu.pl/pl/1/2/18/1/4"
+def main(do_want_to_print):
+    # strona agh:
+    u = "https://sylabusy.agh.edu.pl/pl/1/2/18/1/4"
 
-# wybór wydziału:
-soup = get_info(u)
-departments = get_data(soup)
-u += '/' + chose()
+    # wybór wydziału:
+    soup = get_info(u)
+    departments = get_data(soup)
+    depart = chose()
+    u += '/' + depart
 
-# wybór kierunku:
-soup = get_info(u)
-majors = get_data(soup)
-major = chose()
-u += '/' + major
+    # wybór kierunku:
+    soup = get_info(u)
+    majors = get_data(soup)
+    major = chose()
+    u += '/' + major
 
-# kierunek -> zapis do pliku:
-soup = get_info(u)
-output = list()
-for i in range(7, 14):
-    new_data = soup.find_all("div", {"id": "syl-grid-period-" + str(i)})
-    output.append(write_data(new_data))
+    # kierunek -> pobranie danych (przedzmiotów):
+    soup = get_info(u)
+    output = list()
+    for i in range(7, 14):
+        new_data = soup.find_all("div", {"id": "syl-grid-period-" + str(i)})
+        output.append(write_data(new_data))
 
-try:
-    name = majors[major] + '.txt'
-except:
-    name = "Syllabus.txt"
+    # Zapis do plku:
+    try:
+        name = majors[major] + '.txt'
+    except KeyError:
+        name = "Syllabus.txt"
+    file = open(name, 'w')
+    file.write('>>>' + departments[depart])
+    file.write('\n>>>' + majors[major] + ':')
+    file.write('\n\n')
 
-file = open(name, 'w')
-print(majors)
-print('    ', majors[major])
-file.write('>>>' + majors[major] + ':')
-file.write('\n\n')
+    print(departments[depart] + '\n' + majors[major])  # nazwa kierunku
+    for i in range(len(output)):
+        if do_want_to_print is True:
+            print(output[i])
+        file.write(output[i])
+    print("Courses have been saved in:", name)
 
-for i in range(len(output)):
-    print(output[i])
-    file.write(output[i])
+
+x = input('Do you want me to print courses also in terminal? (choose)[Y/N]')
+if x == 'Y' or x == '1' or x == 'y':
+    want_print = True
+else:
+    want_print = False
+main(want_print)
